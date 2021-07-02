f16855aafdbfa3a1f615c5d855da046ce5b392c3
mrinal1224 <iammrinalkc@gmail.com>
2021-07-02 18:28:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-twggae
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-twggae)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React , {useState} from "react";
+import "./style.css";
+
+export default function App() {
+  const[msgObj , setObject]=useState({message:'' , id:'1'})
+
+  const handleChange=()=>{
+      
+  }
+  return (
+    <div>
+    <input typr='text'>
+
+    </input>
+    </div>
+  );
+}
